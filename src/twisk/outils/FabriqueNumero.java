1619eb93e28a2b4bedbdc3855b2bca767c855e56
lichtner1u <lichtner1u@univ-lorraine.fr>
2023-01-31 16:24:32 +0100

    Ajout du singleton FabriqueNumero

--- src/twisk/outils/FabriqueNumero.java
+++ src/twisk/outils/FabriqueNumero.java
@@ -1,5 +1,46 @@
 package twisk.outils;
 
+/**
+ * @author Diedler et Baptiste
+ * Fabrique des différents paquets de cartes du uno
+ */
 public class FabriqueNumero {
+
+    /**
+     * Compteur d'étapes
+     */
+    private int cptEtapes;
+
+    /**
+     * Instance de FabriqueNumero
+     */
+    private static FabriqueNumero instance = new FabriqueNumero();
+
+    /**
+     *
+     */
+    private FabriqueNumero(){
+        this.cptEtapes = 0;
+    }
+
+    /**
+     * Récupère l'instance du singleton
+     * @return l'instance
+     */
+    public static FabriqueNumero getInstance(){
+        return instance;
+    }
+
+    /**
+     * Donne le numéro de l'étape
+     * @return le numéro de l'étape
+     */
+    public int getNumeroEtape(){
+        return cptEtapes;
+    }
+
+    public void reset(){
+        this.cptEtapes = 0;
+    }
     
 }
