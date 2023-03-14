314497bc0a90bf2e67f390767dfb786d5e2abc99
DIEDLER Baptiste <baptiste.diedler3@etu.univ-lorraine.fr>
2023-03-14 14:57:51 +0100

    correction de commentaire

--- src/twisk/monde/SasSortie.java
+++ src/twisk/monde/SasSortie.java
@@ -5,6 +5,7 @@ package twisk.monde;
  * Cette classe se trouve dans le package twisk.monde et permet de savoir si une étape
  *                      est une sortie ou non. Si l'étape est une sortie alors SasSortie sera successeur
  *                      de cette étape.
+ *
  * @author Diedler et Litchner
  */
 
