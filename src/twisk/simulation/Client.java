295ee0e5e58dbb28d4114638e50d9dd1d8a70b01
DIEDLER Baptiste <baptiste.diedler3@etu.univ-lorraine.fr>
2023-03-28 14:50:36 +0200

    changement de la classe Client et GestionnaireClients

--- src/twisk/simulation/Client.java
+++ src/twisk/simulation/Client.java
@@ -1,4 +1,44 @@
 package twisk.simulation;
 
+import twisk.monde.Etape;
+
+/**
+ * classe représentant les clients du monde
+ */
 public class Client {
+
+    /**
+     * numéro du client
+     */
+    int numeroClient;
+
+    /**
+     * etape dans lequel se trouve le client
+     */
+    Etape etape;
+
+    /**
+     * rang du client dans une file d'attente
+     */
+    int rang;
+
+
+    /**
+     * constructeur de la classe
+     */
+    public Client(int numero){
+        this.numeroClient = numero;
+    }
+
+
+    /**
+     * méthode qui permet de déplacer un client dans une nouvelle étape
+     *
+     * @param etape nouvelle étape du client
+     * @param rang position dans la file d'attente du client
+     */
+    public void allerA(Etape etape, int rang){
+        this.etape = etape;
+        this.rang = rang;
+    }
 }
--- src/twisk/simulation/GestionnaireClients.java
+++ src/twisk/simulation/GestionnaireClients.java
@@ -1,4 +1,54 @@
 package twisk.simulation;
 
-public class GestionnaireClients {
+import twisk.monde.Etape;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * classe qui permet de gérer les instances des clients
+ */
+public class GestionnaireClients implements Iterable<Client>{
+
+    /**
+     * clients se trouvant dans le monde
+     */
+    private Client[] clients;
+
+
+    /**
+     * constructeur de la classe
+     */
+    public GestionnaireClients(){
+    }
+
+
+    /**
+     * méthode qui permet de mettre à jour les clients dans le monde
+     *
+     * @param tabClients tableau des clients se trouvant dans le monde
+     */
+    public void setClients(int ... tabClients){
+        clients = new Client[tabClients.length];
+        for(int i=0; i<clients.length; i++){
+            clients[i] = new Client(tabClients[i]);
+        }
+    }
+
+
+    /**
+     * méthode qui permet de déplacer un client dans une nouvelle étape
+     *
+     * @param numeroDeCleint numero du client qui se déplace
+     * @param etape nouvelle étape du client
+     * @param rang position dans la file d'attente du client
+     */
+    public void allerA(int numeroDeCleint, Etape etape, int rang){
+        clients[numeroDeCleint].allerA(etape, rang);
+    }
+
+    @Override
+    public Iterator<Client> iterator() {
+        return Arrays.stream(clients).iterator();
+    }
 }
