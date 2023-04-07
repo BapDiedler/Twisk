4e7b445b8478bd61f741b48caf7e4541d8300021
DIEDLER Baptiste <baptiste.diedler3@etu.univ-lorraine.fr>
2023-04-07 09:16:09 +0200

    ajout du GuichetIG dans le projet

---
+++ src/twisk/mondeIG/GuichetIG.java
@@ -0,0 +1,24 @@
+package twisk.mondeIG;
+
+/**
+ * la classe permet une représentation d'un guichet (fil d'attente) dans le monde twisk
+ */
+public class GuichetIG extends EtapeIG{
+
+
+    /**
+     * constructeur de la classe guichet
+     *
+     * @param nom nom donné au guichet
+     */
+    public GuichetIG(String nom){
+        super(nom);
+    }
+
+    /**
+     * constructeur vide de la classe
+     */
+    public GuichetIG(){
+        this("guichet");
+    }
+}
