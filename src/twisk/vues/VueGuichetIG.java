3b31fab65d2dc968abea84bf593cb7d74d70ad12
DIEDLER Baptiste <baptiste.diedler3@etu.univ-lorraine.fr>
2023-04-12 19:26:02 +0200

    changement du code dans le guichet

--- src/twisk/vues/VueGuichetIG.java
+++ src/twisk/vues/VueGuichetIG.java
@@ -1,24 +1,30 @@
 package twisk.vues;
 
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 import twisk.mondeIG.EtapeIG;
 import twisk.mondeIG.MondeIG;
 import twisk.outils.TailleComposants;
+
+import java.util.Objects;
 
 /**
  * Décrit le composant graphique d'un guichet
  */
 public class VueGuichetIG extends VueEtapeIG{
 
-    private final HBox filAttente;
-    private Rectangle[] zoneAttente;
+    /**
+     * zone représentant la file d'attente des clients
+     */
+    private final HBox fileAttente;
+    /**
+     * zone représentant l'espace pour un client dans la file d'attente
+     */
+    private final Rectangle[] zoneAttente;
 
     /**
      * Le constructeur de VueGuichetIG
@@ -28,13 +34,13 @@ public class VueGuichetIG extends VueEtapeIG{
      */
     public VueGuichetIG(MondeIG monde, EtapeIG guichet, boolean estCLique){
         super(monde,guichet,estCLique);
-        filAttente = new HBox();
-        this.getChildren().add(filAttente);
+        fileAttente = new HBox();
+        this.getChildren().add(fileAttente);
         zoneAttente = new Rectangle[6];
         for(int i=0; i< zoneAttente.length; i++){
             zoneAttente[i] = new Rectangle(TailleComposants.getLargeurGuichet()*0.8/6,TailleComposants.getLargeurGuichet()*0.8/6);
         }
-        filAttente.getChildren().addAll(zoneAttente);
+        fileAttente.getChildren().addAll(zoneAttente);
         defineStyle();
     }
 
@@ -58,16 +64,19 @@ public class VueGuichetIG extends VueEtapeIG{
                 rectangle.setStroke(Color.BLUE);
             }
         }
-        filAttente.setSpacing(1);
-        filAttente.setAlignment(Pos.CENTER);
+        fileAttente.setSpacing(1);
+        fileAttente.setAlignment(Pos.CENTER);
     }
 
+    /**
+     * méthode qui permet de mettre le nom et les attributs du guichet
+     */
     protected void defineNomLabel(){
         String nom = etape.getNom();
         String jetons = etape.getNbJetons() + " jetons";
         titre.setText(nom + ": " + jetons);
-        Image entree = new Image(getClass().getResourceAsStream("/check-in.png"), 30,30,true,true);
-        Image sortie = new Image(getClass().getResourceAsStream("/check-out.png"), 30,30,true,true);
+        Image entree = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/check-in.png")), 30,30,true,true);
+        Image sortie = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/check-out.png")), 30,30,true,true);
         ImageView entreeView = new ImageView(entree);
         ImageView sortieView = new ImageView(sortie);
         if(etape.estEntreeEtEstSortie()){
