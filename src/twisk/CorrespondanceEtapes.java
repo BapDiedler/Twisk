3430c5cc59ded92a4af69be5811863e72d3adec8
DIEDLER Baptiste <baptiste.diedler3@etu.univ-lorraine.fr>
2023-04-25 09:24:54 +0200

    création de CorrespondanceEtapes

--- src/twisk/CorrespondanceEtapes.java
+++ src/twisk/CorrespondanceEtapes.java
@@ -1,4 +1,42 @@
 package twisk;
 
+import twisk.monde.Etape;
+import twisk.mondeIG.EtapeIG;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * classe qui va faire le lien entre les étapes graphiques et celle du monde
+ */
 public class CorrespondanceEtapes {
+    HashMap<EtapeIG, Etape> etapes;
+
+    /**
+     * constructeur vide
+     */
+    public CorrespondanceEtapes(){
+        etapes = new HashMap<>(10);
+    }
+
+    /**
+     * méthode qui permet d'ajouter des relations entre les étapes
+     *
+     * @param etapeIG étape graphique
+     * @param etape étape
+     */
+    public void ajouter(EtapeIG etapeIG, Etape etape){
+        etapes.put(etapeIG,etape);
+    }
+
+    /**
+     * getter qui permet d'obtenir un Etape à l'aide de l'étape graphique
+     *
+     * @param etapeIG étape graphique
+     *
+     * @return l'étape qui est en relation avec etapeIG
+     */
+    public Etape get(EtapeIG etapeIG){
+        return etapes.get(etapeIG);
+    }
 }
